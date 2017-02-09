22b70844e12d5b148713d9a8062fa9066e775bde
ChadoNihi <chadoishere@gmail.com>
2017-02-09 22:21:03 +0200

    save

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = [
+{
+  context: path.resolve(__dirname),
+  entry:  './src/client.js',
+  output: {
+    path: path.resolve(__dirname, './public/js'),
+    filename: 'client.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: [{
+          loader: 'babel-loader',
+          //options: { presets: ['es2015'] }
+          exclude: [/node_modules/],
+        }],
+      },
+
+      // Loaders for other file types can go here
+    ],
+  },
+},
+
+{
+  target: 'node',
+  context: path.resolve(__dirname),
+  entry:  './src/server.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: 'server.bundle.js',
+  },
+},
+];
